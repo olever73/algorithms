fbd27e51a1f81dc2f00c15fefd1a62e9448dac69
olever <olever73@gmail.com>
2022-04-26 00:15:46 +0200

    add "order_servise"

--- algorithms/22_03_29_orders/src/de/telran/OrderServise.java
+++ algorithms/22_03_29_orders/src/de/telran/OrderServise.java
@@ -32,20 +32,6 @@ import java.util.*;
          }
          return result;
      }
-      /*
-         public int[] countOrdersNumber(long[] orderTimes, int minutes) {
-             int[] res = new int[orderTimes.length];
-             Deque<Long> queue = new ArrayDeque<>();
-             long millis = minutes * 60L * 1000;
-             int i = 0;
-             for (long orderTime : orderTimes) {
-                 queue.addLast(orderTime);
-                 while (orderTime - queue.getFirst() > millis)
-                     queue.removeFirst();
-                 res[i++] = queue.size() - 1;
-             }
-             return res;
-         }
-     */
+
 
  }
